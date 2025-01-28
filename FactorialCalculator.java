9def73471b23fb9089c1c544e3629a29c6911787
Mounika <nagargojemounika@gmail.com>
2025-01-29 05:00:30 +0530

    Add files via upload

---
+++ FactorialCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        long factorial = calculateFactorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+
+        scanner.close();
+    }
+
+    public static long calculateFactorial(int num) {
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
+
